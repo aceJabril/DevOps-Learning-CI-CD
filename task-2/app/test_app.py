d656edd0fc520a73cd1d4897096fcf33838c10a9
Jabril Mahamed <acejabril@jabz-macbook.local>
2026-04-10 17:01:54 +0100

    Fix flake8 formatting

--- task-2/app/test_app.py
+++ task-2/app/test_app.py
@@ -1,11 +1,11 @@
 import unittest
 
+
 class TestApp(unittest.TestCase):
+
     def test_placeholder(self):
         self.assertEqual(1, 1)
 
+
 if __name__ == "__main__":
     unittest.main()
-
-
-
